aa0adb35316b3ee209e6456087ac0e9037c86e84
R ARUNACHALAM <chandrikaarun1958@gmail.com>
2023-11-28 01:48:15 +0530

    "SOME CHANGES"

--- src/Components/AddProduct/AddProduct.jsx
+++ src/Components/AddProduct/AddProduct.jsx
@@ -21,7 +21,7 @@ const AddProduct = () => {
     let formData = new FormData();
     formData.append("product", image);
 
-    await fetch("http://localhost:4000/upload", {
+    await fetch("https://sedona-backend.onrender.com/upload", {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -36,7 +36,7 @@ const AddProduct = () => {
     if (dataObj.success) {
       product.image = dataObj.image_url;
       console.log(product);
-      await fetch("http://localhost:4000/addproduct", {
+      await fetch("https://sedona-backend.onrender.com/addproduct", {
         method: "POST",
         headers: {
           Accept: "application/json",
